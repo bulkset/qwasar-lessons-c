a4343f027fb9b1da46fb0ac213094b6a108da04f
ScaryTools <chupapa327@rambler.ru>
2023-09-25 14:22:06 +0500

    ex08

--- bootcamp-c-arc-01/Quest02/ex08/my_string_index.c
+++ bootcamp-c-arc-01/Quest02/ex08/my_string_index.c
@@ -3,8 +3,9 @@ int my_string_index(char* param_1, char param_2) {
     int index = -1;
     int i = 0;
     
-    while (param_1[i] != '\0') {
-        if (param_1[i] == param_2) {
+    
+    for(i = 0; param_1[i] != '\0';i){
+        if(param_1[i] == param_2){
             index = i;
             break;
         }
